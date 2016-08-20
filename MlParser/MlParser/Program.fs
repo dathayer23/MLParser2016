31ffae9da9b7732aa8cfd1992a1f4c711587a1d9
David Thayer <dpthayer@gmail.com>
2016-08-20 16:49:32 -0700

    tests

--- MlParser/MlParser/Program.fs
+++ MlParser/MlParser/Program.fs
@@ -4,11 +4,22 @@ namespace MLParser
 open Microsoft.FSharp.Text.Lexing
 open ML_Lexer
 
-module main =
+module Main  =
+
+   let readTokens lexbuf  = 
+      let rec getTokens lexbuf tokens = 
+         let tk = tokenize lexbuf
+         match  tk  with
+         | EOF -> List.rev (tk :: tokens)
+         | _ ->  tk :: (getTokens lexbuf tokens)
+
+      getTokens lexbuf []
+
+
    [<EntryPoint>]
    let main argv = 
        printfn "%A" argv
-       let lexbuf = LexBuffer<char>.FromString ("let x y = 1")
-       let tokens = tokenize lexbuf
-
+       let lexbuf = LexBuffer<char>.FromString ("let x y = 1 and do while z < 1.0 end\n f : int -> float")
+       let tokens = readTokens lexbuf
+       
        0 // return an integer exit code
