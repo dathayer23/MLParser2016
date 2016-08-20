bdc66efa5a58b47c0c54d42aa6a9c7693786cff5
David Thayer <dpthayer@gmail.com>
2016-08-20 16:32:25 -0700

    first attempt to lex a statement

--- MlParser/MlParser/Program.fs
+++ MlParser/MlParser/Program.fs
@@ -1,9 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 namespace MLParser
+open Microsoft.FSharp.Text.Lexing
+open ML_Lexer
 
 module main =
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+   [<EntryPoint>]
+   let main argv = 
+       printfn "%A" argv
+       let lexbuf = LexBuffer<char>.FromString ("let x y = 1")
+       let tokens = tokenize lexbuf
+
+       0 // return an integer exit code
